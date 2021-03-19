23eb51ceeb1259b455f8d59ef9c5f10f61a54d92
Júlio César <julio.souzam@gmail.com>
2021-03-19 00:05:36 -0300

    fix: remove token and replace to a env

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'http://www.omdbapi.com',
   params: {
-    apikey: '925eba28',
+    apikey: process.env.REACT_APP_API_KEY,
   },
 });
 
